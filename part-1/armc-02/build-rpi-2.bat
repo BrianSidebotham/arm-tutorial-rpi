be1eb30cee44f9934e03488fb33c68e981dad1d2
BrianSidebotham <brian.sidebotham@gmail.com>
2015-02-19 22:31:15 +0000

    Fix RPI2 compile options
    Add missing -mtune=cortex-a7 to RPI2 build scripts

--- part-1/armc-02/build-rpi-2.bat
+++ part-1/armc-02/build-rpi-2.bat
@@ -1,3 +1,3 @@
 @ECHO OFF
 
-arm-none-eabi-gcc -O2 -DRPI2 -mfpu=neon-vfpv4 -mfloat-abi=hard -march=armv7-a -nostartfiles -g armc-02.c -o kernel.elf
+arm-none-eabi-gcc -O2 -DRPI2 -mfpu=neon-vfpv4 -mfloat-abi=hard -march=armv7-a -mtune=cortex-a7 -nostartfiles -g armc-02.c -o kernel.elf
