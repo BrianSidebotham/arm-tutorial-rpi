1f96dbfa6e7dc11c9bafa9d2122bad5aa46b315c
BrianSidebotham <brian.sidebotham@gmail.com>
2015-04-02 14:06:25 +0100

    Fix bplus build issue (Fixes #4)
    Wrong processor flags and definitions for bplus build. Plus, EOL
    conversion in Linux/Mac build script

--- part-1/armc-03/build-rpi-bplus.bat
+++ part-1/armc-03/build-rpi-bplus.bat
@@ -1,4 +1,4 @@
 @ECHO OFF
 
-arm-none-eabi-gcc -O2 -DRPI2 -mfpu=vfp -mfloat-abi=hard -march=armv7-a -nostartfiles -g armc-03.c -o kernel.elf
+arm-none-eabi-gcc -O2 -DRPIBPLUS -mfpu=vfp -mfloat-abi=hard -march=armv6zk -mtune=arm1176jzf-s -nostartfiles -g armc-03.c -o kernel.elf
 arm-none-eabi-objcopy kernel.elf -O binary kernel.img
