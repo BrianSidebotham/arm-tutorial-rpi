2e8f4ac8b3edd77917abcf13b2c48be7d98a62fa
BrianSidebotham <brian.sidebotham@gmail.com>
2016-01-05 13:35:03 +0000

    Fix building armc-02 for rpi bplus

--- part-1/armc-02/build-rpi-bplus.bat
+++ part-1/armc-02/build-rpi-bplus.bat
@@ -1,3 +1,3 @@
 @ECHO OFF
 
-arm-none-eabi-gcc -O0 -DRPIBPLUS -mfpu=vfp -mfloat-abi=hard -march=armv7-a -nostartfiles -g armc-02.c -o kernel.elf
+arm-none-eabi-gcc -O0 -DRPIBPLUS -mfpu=vfp -mfloat-abi=hard -march=armv6zk -mtune=arm1176jzf-s -nostartfiles -g armc-02.c -o kernel.elf
