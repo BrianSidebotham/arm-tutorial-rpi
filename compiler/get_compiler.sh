c63f6f75269ab5b9824e18249dac2e3b6cb97e87
Brian Sidebotham <brian.sidebotham@gmail.com>
2018-09-21 00:08:25 +0100

    Update the gitignore file to add in some missing parts of the new tutorial layout

---
+++ compiler/get_compiler.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+scriptdir=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)
+
+# Include the common shell functions
+. ${scriptdir}/../shell/common_functions.sh
+
+compiler=gcc-arm-none-eabi-7-2018-q2-update
+archive=${compiler}-linux.tar.bz2
+source=https://armkeil.blob.core.windows.net/developer/Files/downloads/gnu-rm/7-2018q2/${archive}
+
+must_have curl
+must_have md5sum
+must_have tar
+
+if [ ! -f ${scriptdir}/${archive} ]; then
+    must_run curl --fail -o ${scriptdir}/${archive} ${source}
+fi
+
+must_run md5sum -c ${scriptdir}/compiler.md5
+must_run tar xvf ${scriptdir}/${archive}
+
+# Remove the archive after we're finished with it
+rm -rf ${scriptdir}/${archive} > /dev/null 2>&1
+
+# Test the compiler before quitting
+must_run ${scriptdir}/${compiler}/bin/arm-none-eabi-gcc --version
+
+# Create a compiler configuration file
+cat << EOF > "${scriptdir}/.compiler_config"
+#!/bin/sh
+toolchain=${scriptdir}/${compiler}/bin/arm-none-eabi-
+EOF
+
+exit 0
---
+++ compiler/readme.md
@@ -0,0 +1,7 @@
+# Compiler
+
+The `get_compiler.sh` script downloads the supported compiler for the tutorials and sets up a file
+that can be included in shell scripts to access the compiler so you don't need to install any
+other packages.
+
+The current compiler is `gcc-7.3.1`
