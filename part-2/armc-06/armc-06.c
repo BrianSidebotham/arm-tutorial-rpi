8fe6630c65eb2e437901e7fa3e1f6a3bd2839eaa
Brian Sidebotham <brian.sidebotham@gmail.com>
2018-10-15 00:02:02 +0100

    Update part-2 armc-06 for RPI3 and RPI Zero

--- part-2/armc-06/armc-06.c
+++ part-2/armc-06/armc-06.c
@@ -40,12 +40,5 @@ int main(void)
         /* Set the LED GPIO pin high ( Turn OK LED off for original Pi, and on
            for plus models )*/
         gpio[LED_GPSET] = (1 << LED_GPIO_BIT);
-
-        for(tim = 0; tim < 500000; tim++)
-            ;
-
-        /* Set the LED GPIO pin low ( Turn OK LED on for original Pi, and off
-           for plus models )*/
-        gpio[LED_GPCLR] = (1 << LED_GPIO_BIT);
     }
 }
--- part-2/armc-06/disassemble.sh
+++ part-2/armc-06/disassemble.sh
@@ -1,4 +1,20 @@
 #!/bin/sh
 
-arm-none-eabi-objdump -D ./kernel.elf > ./kernel.asm
-arm-none-eabi-nm ./kernel.elf > ./kernel.nm
+scriptdir=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)
+base=${scriptdir}/../..
+
+# Get the compiler to use, etc.
+. ${base}/shell/common_build.sh
+. ${base}/shell/common_functions.sh
+
+
+# Get the tutorial name from the script directory
+tutorial=${scriptdir##*/}
+
+for elf in ${scriptdir}/*.elf; do
+    img=${elf%.elf}.img
+    printf "%s\n" "Disassembling ${elf} to ${elf}.asm"
+    arm-none-eabi-objdump -D ${elf} > ${elf}.asm
+    arm-none-eabi-nm ${elf} > ${elf}.nm
+    hexdump ${img} > ${img}.hexdump
+done
