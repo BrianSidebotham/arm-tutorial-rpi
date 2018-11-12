452c3d1bc2c7199d1f9bd04a62e43088bda0cce8
Brian Sidebotham <brian.sidebotham@gmail.com>
2018-11-12 21:56:51 +0000

    Update part-5 armc-014 to start pi3 support. Currently only tested with rpi0

--- part-5/armc-014/toolchain-arm-none-eabi-rpi.cmake
+++ part-5/armc-014/toolchain-arm-none-eabi-rpi.cmake
@@ -1,29 +1,10 @@
 
 #   Part of the Raspberry-Pi Bare Metal Tutorials
-#   Copyright (c) 2013-2015, Brian Sidebotham
-#   All rights reserved.
+#   https://www.valvers.com/rpi/bare-metal/
+#   Copyright (c) 2013, Brian Sidebotham
 
-#   Redistribution and use in source and binary forms, with or without
-#   modification, are permitted provided that the following conditions are met:
-
-#   1. Redistributions of source code must retain the above copyright notice,
-#       this list of conditions and the following disclaimer.
-
-#   2. Redistributions in binary form must reproduce the above copyright notice,
-#       this list of conditions and the following disclaimer in the
-#       documentation and/or other materials provided with the distribution.
-
-#   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-#   AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-#   IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-#   ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
-#   LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-#   CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-#   SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-#   INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-#   CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-#   ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-#   POSSIBILITY OF SUCH DAMAGE.
+#   This software is licensed under the MIT License.
+#   Please see the LICENSE file included with this software.
 
 # A CMake toolchain file so we can cross-compile for the Rapsberry-Pi bare-metal
 
@@ -47,7 +28,8 @@ set( CROSS_COMPILE arm-none-eabi- )
 # specify the cross compiler. We force the compiler so that CMake doesn't
 # attempt to build a simple test program as this will fail without us using
 # the -nostartfiles option on the command line
-CMAKE_FORCE_C_COMPILER( ${TC_PATH}${CROSS_COMPILE}gcc GNU )
+CMAKE_FORCE_C_COMPILER( "${TC_PATH}${CROSS_COMPILE}gcc" GNU )
+SET( CMAKE_ASM_COMPILER "${TC_PATH}${CROSS_COMPILE}gcc" )
 
 # We must set the OBJCOPY setting into cache so that it's available to the
 # whole project. Otherwise, this does not get set into the CACHE and therefore
@@ -55,13 +37,24 @@ CMAKE_FORCE_C_COMPILER( ${TC_PATH}${CROSS_COMPILE}gcc GNU )
 set( CMAKE_OBJCOPY      ${TC_PATH}${CROSS_COMPILE}objcopy
     CACHE FILEPATH "The toolchain objcopy command " FORCE )
 
+set( CMAKE_OBJDUMP      ${TC_PATH}${CROSS_COMPILE}objdump
+CACHE FILEPATH "The toolchain objdump command " FORCE )
+
+# Set the common build flags
+
 # Set the CMAKE C flags (which should also be used by the assembler!
-set( CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -mfpu=vfp" )
+set( CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -O0" )
+set( CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -g" )
+set( CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -nostartfiles" )
 set( CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -mfloat-abi=hard" )
-set( CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -march=armv6zk" )
-set( CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -mtune=arm1176jzf-s" )
+
+if( "${BOARD}" STREQUAL "rpi0" )
+    set( CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -DRPI0" )
+    set( CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -mfpu=vfp" )
+    set( CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -march=armv6zk" )
+    set( CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -mtune=arm1176jzf-s" )
+endif()
+
 
 set( CMAKE_C_FLAGS "${CMAKE_C_FLAGS}" CACHE STRING "" )
 set( CMAKE_ASM_FLAGS "${CMAKE_C_FLAGS}" CACHE STRING "" )
-
-
