ac396541335d4691297fa3425561fdb15b10b5ec
Brian Sidebotham <brian.sidebotham@gmail.com>
2019-07-25 23:38:14 +0100

    Fix the get_compiler script so it can be called from directories other than itself

--- compiler/get_compiler.sh
+++ compiler/get_compiler.sh
@@ -17,8 +17,14 @@ if [ ! -f ${scriptdir}/${archive} ]; then
     must_run curl --fail -o ${scriptdir}/${archive} ${source}
 fi
 
-must_run md5sum -c ${scriptdir}/compiler.md5
-must_run tar xvf ${scriptdir}/${archive}
+# Generate an MD5 file on the fly - we know the MD5 of the compiler package we're after, and the filename needs to
+# include the full path to the archive
+cat << EOF > ${scriptdir}/.compiler.md5
+299ebd3f1c2c90930d28ab82e5d8d6c0  ${scriptdir}/${archive}
+EOF
+
+must_run md5sum -c ${scriptdir}/.compiler.md5
+must_run tar -C ${scriptdir} -xvf  ${scriptdir}/${archive}
 
 # Remove the archive after we're finished with it
 rm -rf ${scriptdir}/${archive} > /dev/null 2>&1
