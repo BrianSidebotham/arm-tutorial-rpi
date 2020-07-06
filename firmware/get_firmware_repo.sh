13ed0de668bff79812f33667dbcde294ab555def
Brian Sidebotham <brian.sidebotham@gmail.com>
2020-07-06 20:12:41 +0100

    Move to the script directory before cloning firmware

--- firmware/get_firmware_repo.sh
+++ firmware/get_firmware_repo.sh
@@ -2,9 +2,4 @@
 
 scriptdir=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)
 
-if [ -d "${scriptdir}/firmware" ]; then
-  ls -la "${scriptdir}/firmware"
-  rm -rf "${scriptdir}/firmware"
-fi
-
-git clone --depth 1 https://github.com/raspberrypi/firmware.git firmware
+cd ${scriptdir} && git clone --depth 1 https://github.com/raspberrypi/firmware.git firmware
