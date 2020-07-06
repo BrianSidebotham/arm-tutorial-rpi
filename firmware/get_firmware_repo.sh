72297e460b7bbbe2c4fae9f479fa3c85ea5d3dc1
Brian Sidebotham <brian.sidebotham@gmail.com>
2020-07-06 10:08:33 +0100

    Update firmware script to remove firmware directory if necessary

--- firmware/get_firmware_repo.sh
+++ firmware/get_firmware_repo.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
+scriptdir=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)
+
+if [ -d "${scriptdir}/firmware" ]; then
+  rm -rf "${scriptdir}/firmware"
+fi
+
 git clone --depth 1 https://github.com/raspberrypi/firmware.git firmware
