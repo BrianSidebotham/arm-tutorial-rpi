6315cb3836e738a6c03beb2a6b01248509eabb61
Brian Sidebotham <brian.sidebotham@gmail.com>
2020-07-07 22:21:01 +0100

    Update tutorial compiler to 9.2-2019.12 (#25)

--- shell/common_build.sh
+++ shell/common_build.sh
@@ -11,6 +11,13 @@ __valvers_arm_rpi_common_build="included"
 compiler_config_file=${basedir}/compiler/.compiler_config
 if [ -f ${compiler_config_file} ]; then
     . ${compiler_config_file}
+    if [ "${toolchain_version}X" != "X" ]; then
+        if [ "${toolchain_version}" != "${toolchain_latest_version}" ]; then
+            echo "WARNING: The toolchain version your using (${toolchain_version}) is not the latest version" >&2
+            echo "         for the tutorial. Please run the compiler/get_compiler.sh script again to update" >&2
+            echo "         your compiler verison" >&2
+        fi
+    fi
 else
     # Drop back to hoping the compiler is in the PATH
     toolchain=arm-none-eabi-
